ed9293e06bb38fbde855cc2b5c03cb5b524ca73a
sha <35295360+sharathb37@users.noreply.github.com>
2022-04-18 11:17:23 +0530

    Initi

---
+++ CachingService/Cache.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace CachingService
+{
+    public class Cache
+    {
+        // Add required properties filter attributes as required
+    }
+}
---
+++ CachingService/Controllers/CacheController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CachingService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CacheController : ControllerBase
+    {
+        private readonly ILogger<CacheController> _logger;
+
+        public CacheController(ILogger<CacheController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Cache> GetCache()
+        {
+            throw new NotImplementedException("Not implemented exception");
+        }
+
+        [HttpPost]
+        public IEnumerable<Cache> AddCache()
+        {
+            throw new NotImplementedException("Not implemented exception");
+        }
+    }
+}
